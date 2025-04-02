3522c4ac0fb8d35a195d2c8bd364bc649f1dd325
hasanemam1717 <hasanemam1717@gmail.com>
2025-04-02 17:39:55 +0600

    added eslint and pretteir setup

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-vars": "error",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+    },
+  },
+  {
+    ignores: [".node_modules/*"],
+  },
+
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors'
 const app = express();
 
 // using parser
 app.use(express.json());
+app.use(cors());
+const a = 50
 
 // using router 
 // app.use('/api', router)
